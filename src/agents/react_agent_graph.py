cea963c3bfcf6630f79ff22831167f747181005c
Dodero10 <truongdat.work@gmail.com>
2025-05-22 18:51:32 +0700

    refactor: refactor react agent

--- src/agents/react_agent_graph.py
+++ src/agents/react_agent_graph.py
@@ -35,17 +35,7 @@ langfuse = Langfuse(
 langfuse_handler = CallbackHandler()
 
 async def react_agent(state: State) -> Dict[str, List[AIMessage]]:
-    """Call the LLM powering our "agent".
 
-    This function prepares the prompt, initializes the model, and processes the response.
-
-    Args:
-        state (State): The current state of the conversation.
-        config (RunnableConfig): Configuration for the model run.
-
-    Returns:
-        dict: A dictionary containing the model's response message.
-    """
     configuration = Configuration(
         model="openai/gpt-4o-mini",
         system_prompt=REACT_AGENT_PROMPT,
@@ -75,25 +65,16 @@ async def react_agent(state: State) -> Dict[str, List[AIMessage]]:
 
 
 def route_model_output(state: State) -> Literal["__end__", "tools"]:
-    """Determine the next node based on the model's output.
 
-    This function checks if the model's last message contains tool calls.
-
-    Args:
-        state (State): The current state of the conversation.
-
-    Returns:
-        str: The name of the next node to call ("__end__" or "tools").
-    """
     last_message = state.messages[-1]
     if not isinstance(last_message, AIMessage):
         raise ValueError(
             f"Expected AIMessage in output edges, but got {type(last_message).__name__}"
         )
-    # If there is no tool call, then we finish
+
     if not last_message.tool_calls:
         return "__end__"
-    # Otherwise we execute the requested actions
+
     return "tools"
 
 
